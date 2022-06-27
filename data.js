03690c83d5382edc45b06eb76fbbff1a6b5ce6db
deddy <haicoders12@gmail.com>
2022-06-27 17:59:49 +0700

    wip

---
+++ data.js
@@ -0,0 +1,1 @@
+const data = ['heloo', 'word'];
--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-console.log('haloo aku depa');
-console.log('haloo aku depa');
+var today = new Date();
+
+var date =
+  today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+
+var time =
+  today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+
+var dateTime = date + ' ' + time;
