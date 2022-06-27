333008747d96e1abdd283ba3aa62bbd4d5ae2247
deddy <haicoders12@gmail.com>
2022-06-27 19:04:33 +0700

    WIP

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 var today = new Date();
+var date =
+  today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
 
+var time =
+  today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+
+var dateTime = date + ' ' + time;
+var today = new Date();
+var date =
+  today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+
+var time =
+  today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+
+var dateTime = date + ' ' + time;
+var today = new Date();
 var date =
   today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
 
